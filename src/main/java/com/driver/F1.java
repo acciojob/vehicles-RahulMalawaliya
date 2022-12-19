72fc44e177c657641645c86ccf9ca2adc728338b
Rahul <malawaliyarahul@gmail.com>
2022-12-19 11:51:41 +0530

    coderunner

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -22,30 +22,6 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
-        if(newSpeed>0 && newSpeed<=50)
-        {
-            setGears(1);
-        }
-        else if(newSpeed>50 && newSpeed<=100)
-        {
-            setGears(2);
-        }
-        else if(newSpeed>100 && newSpeed<=150)
-        {
-            setGears(3);
-        }
-        else if(newSpeed>150 && newSpeed<=200)
-        {
-            setGears(4);
-        }
-        else if(newSpeed>200 && newSpeed<=250)
-        {
-            setGears(5);
-        }
-        else if(newSpeed>250 && newSpeed<=300)
-        {
-            setGears(6);
-        }
 
         if(newSpeed == 0 ) {
             //Stop the car, set gear as 1
@@ -53,6 +29,15 @@ public class F1 extends Car {
            changeGear(1);
         }
         //for all other cases, change the gear accordingly
+        if(newSpeed>250) changeGear(6);
+
+        else if(newSpeed>200) changeGear(5);
+
+        else if(newSpeed>150) changeGear(4);
+
+        else if(newSpeed>100)changeGear(3);
+
+        else if(newSpeed>50)changeGear(2);
 
         if(newSpeed > 0) {
             changeSpeed(newSpeed, getCurrentDirection());
